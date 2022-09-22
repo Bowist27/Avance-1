b3d232d8d8854825de3854d4590d49367df65847
Bowist27 <111481792+Bowist27@users.noreply.github.com>
2022-09-22 14:31:01 -0500

    Update Calcufacil.py

--- Calcufacil.py
+++ Calcufacil.py
@@ -3,15 +3,12 @@
 '''
 '''
 import tkinter as tk
-
 #Colores Primera Parte
 fondo_entrar = "#F51487"
 fondo_menu = "#6DE1E4"
 fondo_incorrecto = "#F51487"
 fondo_entradausercontra = "#6DE1E4"
 fondo_entradausercontra = "#FA1A1A"
-
-
 ventana = tk.Tk()
 ventana.title('Login')
 ventana.geometry('1800x950')
@@ -20,7 +17,6 @@ fondo = tk.PhotoImage(file='Calcula-Fácil (1).png')
 fondo1 = tk.Label(ventana, image=fondo).place(x=0, y=0, relwidth=1, relheight=1 )
 # En el caso de error de contraseña, se dispondra de 2 intentos más, despues de eso terminará el programa
 #Letra Montserrat 30
-
 #Creacion de Botones
 botón1 = tk.Button(ventana, text='Entrar', cursor='hand2', bg = fondo_entrar, width= 10, relief='flat', font=("Comic Sans MS", 20, "bold"))
 botón1.place(x=1450, y=800)
@@ -29,11 +25,8 @@ botón1.place(x=1450, y=800)
 # Se deja pendiente el tutorial de la interfaz gráfica
 
 '''
-
 lbl_Bienvenida=Label(text='Bienvenido a la calculadora de impuestos selecciona el ')
-
 #Creación de Etiquetas de texto
-
 #Creación de Menús
 barraMenu=Menu(ventana)
 mnuArchivo=Menu(barraMenu)
@@ -47,10 +40,7 @@ mnuArchivo.add_separator()
 mnuArchivo.add_command(label='Salir')
 barraMenu.add_cascade(label='Archivo',menu=mnuArchivo)
 mnuArchivo.add_checkbutton(delattr(label))
-
 ventana.config(menu=barraMenu)
-
-
 ventana.mainloop()
 '''
 
@@ -69,33 +59,17 @@ ventana.mainloop()
 
 #Se hacen funciones pues al seleccionar un boton en la interfaz gráfica este iniciará la función
 
+
 def Bienvenida ():
     print('Bienvenido al sistema de calculación de impuestos 2070')
 
 #Se hizo uso de un usuario y contraseña para poder acceder al codigo, se hizo uso de un for in range para dar limite de 3 intentos.
 
-def Usuario_Contraseña():
-    Usuario1='Usuario1'
-    Usuario2='Usuario2'
-    contraseña1="25632"
-    contraseña2="36985"
-    for i in range(1,4):
-        d=input("USUARIO: ")
-        c=input("CONTRASEÑA: ")
-        if c == contraseña1 or c == contraseña2 or d == Usuario1 or d == Usuario2:
-            print("\nEl Usuario y contraseña son correctos!!!\n")
-            break
-        else:
-            print(f"\nLa Contraseña o el Usuario es Incorrecta, alejate niño. Lleva {i} intento/s de 3\n")
-            if i == 3:
-                print("\nHA FALLADO LOS 3 INTENTOS SALGA DEL PROGRAMA Y VUELVA A INTENTAR\n")
-                break
-
 
 
 # La prediccion se hace al principio para despues comparar su resultado de un unico impuesto o suma de estos.
 def Predicción_cliente ():
-    predicción = float(input('Cuanto crees que pagarás de impuestos al finalizar el cálculo?')) 
+    predicción_2 = float(input('Cuanto crees que pagarás de impuestos al finalizar el cálculo?')) 
 
 #Se hacen funciones pues al seleccionar un boton en la interfaz gráfica este iniciará la función
 
@@ -118,26 +92,35 @@ def Predicción_Salario():
 
 
 
+#Aun así cuenta con errores gramáticales 
 
 def Predicción_Productos():
     producto= input('Tu producto es libro, joyeria o arte')
-    if producto == ('Si') or producto == ('si'):
+    if producto == ('Si') or producto == ('si') or producto == ('S'):
         print('No pagas Impuesto de ese producto')
+    elif producto == ('libro') or producto == ('libr') or producto == ('librp'):
+        print('No pagas Impuesto de ese producto')
+
+    elif producto == ('joyeria') or producto == ('joyería') or producto == ('joyeri'):
+        print('No pagas Impuesto de ese producto')
+
+    elif producto == ('arte') or producto == ('art') or producto == ('rte'):
+        print('No pagas Impuesto de ese producto')
+        
     else:
         precioproducto= int(input('Cuánto te costó el producto?'))
         precioproductofinal= precioproducto*.16
-        sumaprecios= precioproductofinal + precioproducto
-        print('El producto cuenta con un precio de impuestos de',sumaprecios, 'pesos')
-
+        precioimpuestos = precioproductofinal-precioproducto
+        print('El producto cuenta con un precio de impuestos de',precioimpuestos,'pesos, quedando entonces el producto con un precio total de',precioproductofinal, 'pesos')
 
 def Predicción_Productos_Importados():
     print('Esta area sigue en mantenimiento, vuelva pronto :D')
 
 
-
-
 # Se asigna un menú para que este diga que tipo de impuestos va a querer calcular. 
+
 #Propuesta de red neuronal para ingresar datos como dibujo y que los detecte como numeros en el menú
+
 
 def menu():
     print('Este es el Menu principal seleccione la opcion que esté buscando')
@@ -150,29 +133,44 @@ def menu():
 
 # Al querer calcular mas impuestos, este entrará en un ciclo While True, esto debido a que en la interfaz grafica se podra seguri calculando sin necesidad de cerrar y volver a iniciar el programa.
 
+
+# Los usuarios van a ser algunos que yo ponga en una pagina y automaticamente se iran actualizando, necesita el uso de conexion en linea
+
+#Contraseñas y usurios que vengan de una base de datos en línea
+
 def main ():
     Bienvenida()
-    Usuario_Contraseña()
-    Predicción_cliente()
-    while(True):
-        menu()
-        opcion = (input( ))
-
-        if opcion=='1':
-            Predicción_Salario()
-        
-        elif opcion == '2':
-            Predicción_Productos()
-
-        elif opcion == '3':
+    Usuario1 ='Usuario1'
+    Usuario2 ='Usuario2'
+    contraseña1 ="25632"
+    contraseña2 ="36985"
+    for i in range(1,4):
+        user = input( "USUARIO: ")
+        password = input( "CONTRASEÑA: ")
+        if password == contraseña1 or password == contraseña2 or user == Usuario1 or user == Usuario2:
+            print("\nEl Usuario y contraseña son correctos!!!\n")
             Predicción_cliente()
-            
-        elif opcion == '4':
-            print("Adios, tenga buen día")
-
-            break
+            while(True):
+                menu()
+                opcion = (input( ))
+                if opcion=='1':
+                    Predicción_Salario()
         
+                elif opcion == '2':
+                    Predicción_Productos()
+
+                elif opcion == '3':
+                    Predicción_cliente()
+            
+                elif opcion == '4':
+                    print("Adios, tenga buen día")
+                    break
+                else:
+                    print("Por favor elija una opción correcta")     
         else:
-            print("Por favor elija una opción correcta")
+            print(f"\nLa Contraseña o el Usuario es Incorrecta, alejate niño. Lleva {i} intento/s de 3\n")
+            if i == 3:
+                print("\nHA FALLADO LOS 3 INTENTOS SALGA DEL PROGRAMA Y VUELVA A INTENTAR\n")
+                break
   
 main()
